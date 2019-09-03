65f75f59efef4c49cc12196a7d0e9ef4a7924f2f
Filipe Firmino <filipefirmino@gec.inatel.br>
2019-09-03 07:24:01 -0300

    Put the price in HB
    Signed-off-by: Filipe Firmino <filipefirmino@gec.inatel.br>

--- src/app/+home-broker/home-broker-price/home-broker-price.component.ts
+++ src/app/+home-broker/home-broker-price/home-broker-price.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//import swal from 'sweetalert';
+
 
 @Component({
   selector: 'app-home-broker-price',
@@ -12,4 +14,24 @@ export class HomeBrokerPriceComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  addPrice() {
+    //alert("Escolha o código da ação")
+    /*
+    swal({
+      title: "Are you sure?",
+      text: "<input></input>",
+      icon: "warning",
+    })
+    .then((willDelete) => {
+      if (willDelete) {
+        swal("Poof! Your imaginary file has been deleted!", {
+          icon: "success",
+        });
+      } else {
+        swal("Your imaginary file is safe!");
+      }
+    });
+    */
+  }
 }
